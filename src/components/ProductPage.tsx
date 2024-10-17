7a883c28804bf06784ef722f7ef099f87d1c36ed
YADAV <amit-kumar.yadav@capgemini.com>
2024-10-17 15:57:18 +0530

    test updates

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,10 +1,10 @@
 // src/components/ProductPage.tsx
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProducts } from '../redux/productSlice';
-import { addToCart } from '../redux/cartSlice';
-import { RootState, AppDispatch } from '../redux/store';
-import '../style/productPage.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../redux/productSlice";
+import { addToCart } from "../redux/cartSlice";
+import { RootState, AppDispatch } from "../redux/store";
+import "../style/productPage.css";
 
 const ProductPage: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -19,18 +19,20 @@ const ProductPage: React.FC = () => {
     <div>
       <h1>Products</h1>
       {loading && <p>Loading...</p>}
-      <div className="product-grid">
-        {products.map((product: any) => (
-          <div className="product-card" key={product.id}>
-            <img src={product.image} alt={product.name} className="product-image" />
-            <h3>{product.name}</h3>
-            <p>Price: ${product.price}</p>
-            <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
-          </div>
-        ))}
+      <div data-testid="product-grid" className="product-grid">
+        {products && products.length > 0
+          ? products.map((product: any, index: any) => (
+              <div key={product.id} className="product-item">
+                <img src={product.imageUrl} alt={product.name} />
+                <h2>{product.title}</h2>
+                <p>${product.price}</p>
+              </div>
+            ))
+          : !loading && <p>No products available.</p>}
       </div>
     </div>
   );
 };
 
 export default ProductPage;
+``
